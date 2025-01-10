f20b87109308a7de2df0a6d621574670d683e2fa
Felipe Baracaldo <felipe.baracaldo1118@icloud.com>
2025-01-10 11:23:53 -0500

    cambios en skills

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
 
 const menu = document.querySelector('#menu-icon')
@@ -20,3 +25,21 @@ sr.reveal('.hero-img', {delay: 450, origin: 'top'})
 sr.reveal('.redes', {delay: 300, origin: 'left'})
 sr.reveal('.ri-arrow-down-double-line', {delay: 450, origin: 'right'})
 sr.reveal('.certificados', {delay: 300, origin: 'top'})
+
+/**
+ * skills toggle
+ */
+
+const toggleBtnBox = document.querySelector("[data-toggle-box]");
+const toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+const skillsBox = document.querySelector("[data-skills-box]");
+
+for (let i = 0; i < toggleBtns.length; i++) {
+  toggleBtns[i].addEventListener("click", function () {
+
+    elemToggleFunc(toggleBtnBox);
+    for (let i = 0; i < toggleBtns.length; i++) { elemToggleFunc(toggleBtns[i]); }
+    elemToggleFunc(skillsBox);
+
+  });
+}
